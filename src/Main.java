2f116c464af8c08f6abfb7f600c36a0125bfb896
ngthgph <ngthgph112@gmail.com>
2023-03-13 17:37:59 +0700

    added pseudocode

--- src/Main.java
+++ src/Main.java
@@ -10,4 +10,22 @@ public class Main {
     }
 }
 
-//
+/* PSEUDOCODE:
+Input: N queen, N board
+Output: display all combinations of N queen positions (column positions array with index corresponding to row position)
+
+conditions: rows, columns and diagonals = false
+
+base case 1: all N queens put to N rows (starting row == N) => print result, return
+
+base case 2: no cell to put the next queen (check the conditions in rows, columns and diagonal => return
+
+recursive case:
+test(N, starting row)
+1. Repeat check column i from 1 to N
+	a. Put column i in result set (x[]) => set the corresponding rows, columns and diagonals
+	b. If i satisfy conditions,
+	    i. if i == N, display it
+	    ii. else
+            recursively call test with the test(N, starting row + 1)
+2. Remove column from result set, reset the corresponding rows, columns and diagonals */
